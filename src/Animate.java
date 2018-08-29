d3f0a9c31cbe64119c620dcf59ec9d2b72abe655
RedactedFromPrint <32402605+RedactedFromPrint@users.noreply.github.com>
2018-08-29 11:59:01 -0700

    8/29/18

--- src/Animate.java
+++ src/Animate.java
@@ -6,13 +6,15 @@ final public class Animate {
     JFrame frame;
     DrawPanel drawPanel;
 
-    private int oneX = 200; // Starting X coordinate
-    private int oneY = 200; // Starting Y coordinate
-
-    boolean up = false;
-    boolean down = true;
-    boolean left = false;
-    boolean right = true;
+    private int x1 = 372;
+    private int x2 = 398;
+    private int x3 = 463;
+    private int x4 = 488;
+    private int y1 = 272;
+    private int y2 = 268;
+    
+    private int xInc = 7;
+    private int yInc = 1;
 
     public static void main(String[] args) {
         new Animate().go();
@@ -30,7 +32,7 @@ final public class Animate {
         frame.setResizable(false);
         frame.setSize(851, 480);
         frame.setLocation(250,100);
-        moveDot();
+        drive();
     }
 
     class DrawPanel extends JPanel {
@@ -45,67 +47,54 @@ final public class Animate {
             g.fillRect(0, 0, this.getWidth(), this.getHeight());
             
             //Road Base
-            int height = this.getHeight();
             g.setColor(new Color(36,171,203));
             int[] xBase = {0,0,398,463,845,845};
-            int[] yBase = {480,326,268,268,326,480};
+            int[] yBase = {480,326,268,268,322,480};
             g.fillPolygon(xBase, yBase, 6);
             
             //Moving Road
             g.setColor(new Color(30,200,178));
-            int x1 = 0;
-            int x2 = 312;
-            int x3 = 547;
-            int x4 = 845;
-            int y1 = 326;
-            int y2 = 299;
             int[] xMoving = {x1,x2,x3,x4};
             int[] yMoving = {y1,y2,y2,y1};
             g.fillPolygon(xMoving, yMoving, 4);
             
         }
     }
-
-    private void moveDot() {
+//need 5 road pieces on screen at a time
+    private void drive() {
         while(true){
             checkBounds();
-            moveLoc();
+            moveRoad();
             try{
-                Thread.sleep(10);
+                Thread.sleep(30);
             } catch (Exception exc){}
             frame.repaint();
         }
     }
-    private void moveLoc(){
-    	if(up){
-            oneY-=1;
-        }
-        if(down){
-            oneY+=1;
-        }
-        if(left){
-            oneX-=1;
-        }
-        if(right){
-            oneX+=1;
-        }	
+    private void moveRoad(){
+    	x1 -= xInc*2;
+    	x2 -= xInc;
+    	x3 += xInc;
+    	x4 += xInc*2;
+    	y1 += yInc*2;
+    	y2 += yInc*1;
+    	
     }
     private void checkBounds(){
-    	if(oneX >= 583){
-            right = false;
-            left = true;
-        }
-        if(oneX <= 7){
-            right = true;
-            left = false;
-        }
-        if(oneY >= 559){
-            up = true;
-            down = false;
-        }
-        if(oneY <= 7){
-            up = false;
-            down = true;
-        }	
+    	if (y1 % 5 == 0) {
+    		xInc *= 2;
+    		yInc *= 2;
+    	}
+    	
+    	if (y2 > 474) {
+    		x1 = 372;
+    	    x2 = 398;
+    	    x3 = 463;
+    	    x4 = 488;
+    	    y1 = 272;
+    	    y2 = 268;
+    	    xInc = 7;
+    	    yInc = 1;
+    	}
     }
 }
